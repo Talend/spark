a2f709a69a015cf160e881bdf3dc1fd1a952789e
ossamaka <okafih@talend.com>
2021-01-13 12:16:09 +0100

    add dpp custom build scripts
    add dpp custom build scripts
    
    add dockerfile with tsbi image
    
    add sh script to build spark jars
    
    remove unused modules
    
    use latest TSBI tags
    
    remove examples
    
    chore(TPRUN-1011) : use TSBI 2.6.7
    
    feat(TPRUN-1107): use TSBI 2.7.1 for dockerFile
    
    feat(TPRUN-1107): use TSBI 2.7.2 for dockerFile
    
    chore : use TSBI 2.9.19
    
    change codice to https
    
    latest TSBI & jackson 2.13
    
    Update the version of jackson databind
    
    Bump TSBI to 2.10.3
    
    Bump Jackson to 2.13.3
    
    Use Rocky Linux - TSBI 3.0.4

---
+++ build_spark_image.sh
@@ -0,0 +1,21 @@
+set -e
+
+ARTIFACT_NAME=spark-3.2.0-bin-talend-spark.tgz
+TEMP_FOLDER=spark
+DOCKER_IMAGE_NAME=talend/spark
+DOCKER_IMAGE_TAG=3.2.0-tsbi-3.0.4-tlnd
+
+## build spark targz
+./dev/make-distribution.sh --name talend-spark --tgz -Pyarn -Phive-provided -Porc-provided -Pparquet-provided -Dscala-2.12 -DskipTests
+
+## remove example from targz
+mkdir -p $TEMP_FOLDER
+tar -xzvf $ARTIFACT_NAME -C $TEMP_FOLDER --strip-components 1
+rm -rf $TEMP_FOLDER/lib/spark-examples* $TEMP_FOLDER/data $TEMP_FOLDER/examples $TEMP_FOLDER/python
+
+# build docker image
+docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+
+# clean up
+rm -rf $ARTIFACT_NAME
+rm -rf $TEMP_FOLDER
